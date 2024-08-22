3ae71f95a1f6aa9b1ffdb6e34e97ccc2121db4cc
liamquinn14 <liamq601@gmail.com>
2024-08-22 21:13:40 +0100

    build create/join lobby screen

---
+++ client/src/app/play/create-lobby/page.tsx
@@ -0,0 +1,7 @@
+export default function createLobby() {
+    return (
+        <div className="flex flex-col items-center justify-center w-screen h-dvh bg-zinc-950">
+            <h1 className="text-3xl text-yellow-300 font-semibold w-4/5 md:w-2/3 text-center mb-4">Create Lobby</h1>
+        </div>
+    )
+}
