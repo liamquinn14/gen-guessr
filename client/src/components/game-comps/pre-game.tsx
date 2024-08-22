367206dc269caa737ac84fd95e14d03445dbb1ba
liamquinn14 <liamq601@gmail.com>
2024-08-23 00:09:42 +0100

    wip

--- client/src/components/game-comps/pre-game.tsx
+++ client/src/components/game-comps/pre-game.tsx
@@ -1,7 +1,7 @@
 "use client"
-
 import NotFound from "@/app/play/lobbies/[lobbyId]/not-found";
 import { useSocket } from "../../../hooks/useSocket";
+import { Button } from "../ui/button";
 
 export default function PreGame() {
 
@@ -12,11 +12,15 @@ export default function PreGame() {
     const { state } = socket;
     const { players, mode, id } = state!;
 
-    const playerElements = players.map((player, key) => {
+    const playerElements = players.map((player) => {
         return (
-            <p key={key} className="text-zinc-50 text-xl w-3/4 md:w-1/2 capitalize text-center">{player.toString()}</p>
+            <p key={player.id} className="text-zinc-50 text-xl w-3/4 md:w-1/2 capitalize text-center">{player.name}</p>
         )
     })
+
+    function startGame() {
+        console.log("Starting game...");
+    }
 
     return (
         <div className="flex flex-col items-center justify-center w-screen h-dvh bg-zinc-950">
@@ -26,6 +30,7 @@ export default function PreGame() {
             <p className="text-zinc-100 text-xl mt-4 w-3/4 md:w-1/2 text-center capitalize">Mode: {mode}</p>
             <h3 className="text-yellow-300 text-xl mt-6 text-center font-semibold mb-2">Players:</h3>
             {playerElements}
+            <Button onClick={startGame}>START GAME</Button>
         </div>
     )
 }
