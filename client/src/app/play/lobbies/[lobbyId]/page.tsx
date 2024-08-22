50618e7f963e2c884ddf3be666b88f6975aacfcf
liamquinn14 <liamq601@gmail.com>
2024-08-23 00:00:20 +0100

    fix lobby comp

--- client/src/app/play/lobbies/[lobbyId]/page.tsx
+++ client/src/app/play/lobbies/[lobbyId]/page.tsx
@@ -1,4 +1,12 @@
+"use client"
 import NotFound from "./not-found"
+import PreGame from "@/components/game-comps/pre-game"
+import ImgAnswer from "@/components/game-comps/img-answer"
+import ImgQuestion from "@/components/game-comps/img-question"
+import TextAnswer from "@/components/game-comps/text-answer"
+import TextQuestion from "@/components/game-comps/text-question"
+import Scoreboard from "@/components/game-comps/scoreboard"
+import { useSocket } from "../../../../../hooks/useSocket"
 interface LobbyProps {
     params: {
         lobbyId: string
@@ -17,6 +25,12 @@ export default function Lobby({ params }: LobbyProps) {
 
     return (
         <>
+        { gameScreen === "pre-game" && <PreGame /> }
+        { gameScreen === "img-question" && <ImgQuestion /> }
+        { gameScreen === "img-answer" && <ImgAnswer /> }
+        { gameScreen === "text-question" && <TextQuestion /> }
+        { gameScreen === "text-answer" && <TextAnswer /> }
+        { gameScreen === "scoreboard" && <Scoreboard /> }
         </>   
     )
 }
