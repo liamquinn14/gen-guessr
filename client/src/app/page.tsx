824a9cce1641e118235c53a943338303c78cd3cd
liamquinn14 <liamq601@gmail.com>
2024-08-22 21:03:36 +0100

    install shadcn and finish splash

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-dvh bg-zinc-950">
       <h1 className="text-3xl text-yellow-300 font-semibold w-4/5 md:w-2/3 text-center mb-4">Gen Guessr</h1>
-      <h2 className="text-xl italic text-zinc-100 w-3/4 md:w-1/2 text-center">Game to identify AI-generated content</h2>
+      <h2 className="text-xl italic text-zinc-100 w-3/4 md:w-1/2 text-center mb-8">Game to identify AI-generated content</h2>
+      <Link href="/play">
+        <Button>START GAME</Button>
+      </Link>
     </div>
   );
 }
